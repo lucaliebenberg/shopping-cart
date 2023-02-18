5bfea504b61781d8b4f9ce70da9cf71ebe430cee
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-18 11:29:06 +0200

    /Cart added + removeFromCart action (sucess)

--- src/Products/ProductsList.tsx
+++ src/Products/ProductsList.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { addToCart } from "../Cart/cart.slice";
 import { useAppDispatch } from "../store.hooks";
-import { getProductsSelector, removeProduct } from "./products.slice";
+import { getProductsSelector, Product, removeProduct } from "./products.slice";
 
 const ProductsList: React.FC = () => {
   const products = useSelector(getProductsSelector);
@@ -11,12 +12,15 @@ const ProductsList: React.FC = () => {
     dispatch(removeProduct(id));
   };
 
+  const addToCartHandler = (product: Product) => dispatch(addToCart(product));
+
   return (
     <div>
       <h2>Games List</h2>
       {products.map((product) => (
         <div key={product.id}>
           <span>{`${product.title} : ${product.price}`}</span>
+          <button onClick={() => addToCartHandler(product)}>Add To Cart</button>
           <button onClick={() => removeFromStore(product.id)}>
             Remove from the store
           </button>
